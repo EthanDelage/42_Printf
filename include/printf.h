56b1463deee884159aaa9dd4cca0bc7e1e283569
edelage <ethandelage04@gmail.com>
2023-02-02 00:37:50 +0100

    Start project

---
+++ include/printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ethan <ethan@student.42lyon.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/02 00:33:00 by ethan             #+#    #+#             */
+/*   Updated: 2023/02/02 00:33:00 by ethan            ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef PRINTF_H
+# define PRINTF_H
+
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int	ft_printf(const char *format, ...);
+
+#endif
